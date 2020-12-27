907f89abe6d7e1ba527f346d92fe9a780b18b580
Olivier Scherrer <pode.fr@gmail.com>
2020-12-27 20:43:01 +0100

    Mise en page de la page contact

---
+++ site/navigation.js
@@ -0,0 +1,25 @@
+// highlight element de navigation selectionné
+let currentPage;
+
+window.addEventListener('hashchange', function () {
+  hideSection(currentPage);
+  currentPage = window.location.hash;
+  showSection(currentPage);
+});
+
+window.addEventListener('load', function () {
+  const id = window.location.hash || 'accueil';
+
+  currentPage = id;
+  showSection(id)
+});
+
+function showSection(id) {
+  document.querySelector(`a[href='${id}']`).classList.add('selected');
+  document.querySelector(`${id}`).classList.add('visible');
+}
+
+function hideSection(id) {
+  document.querySelector(`a[href='${id}']`).classList.remove('selected');
+  document.querySelector(`${id}`).classList.remove('visible');
+}
